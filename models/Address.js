cd1d2c18608ccbcd516abb8b8ac8b9bf8dd82f57
Sukanya Sen <76467704+SukanyaSen530@users.noreply.github.com>
2022-04-06 11:16:20 +0530

    feat - added address schema

--- models/Address.js
+++ models/Address.js
@@ -2,7 +2,28 @@ import mongoose from "mongoose";
 import User from "./User.js";
 const { Schema, model } = mongoose;
 
-const AddressSchema = new Schema({});
+const AddressSchema = new Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: [true, "User Id is required!"],
+    ref: User,
+  },
+  name: {
+    type: String,
+    required: [true, "Reciever name is required for address"],
+  },
+  area: String,
+  city: String,
+  state: String,
+  country: String,
+  pinCode: String,
+  mobile: String,
+  addressType: {
+    type: String,
+    enum: ["home", "work"],
+    default: "home",
+  },
+});
 
 const Address = model("address", AddressSchema);
 
